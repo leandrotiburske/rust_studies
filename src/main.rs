2fb5c9c02a08c39921435ecd413edd596ed676c7
Leandro Tiburske <tiburskeleandro@gmail.com>
2024-04-19 10:43:38 -0300

    finished guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-// use standard input/output library
-use std::io;
+// use standard input/output library and the Ordering type
+use std::{cmp::Ordering, io};
 
 // use the rand library to get random numbers
 use::rand::Rng;
@@ -10,14 +10,28 @@ fn main() {
 
     // get a random number between 1 and 100
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    // print random number
-    println!("The secret number is: {secret_number}");
 
-    println!("Please input your guess.");
-    let mut guess = String::new();
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    println!("You guessed: {guess}");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        println!("You guessed: {guess}");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
