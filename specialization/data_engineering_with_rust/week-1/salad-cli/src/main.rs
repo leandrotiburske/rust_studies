dfdd629601a22e3c2bdd652eae79d44635489052
Leandro Tiburske <tiburskeleandro@gmail.com>
2024-05-08 16:35:48 -0300

    uploaded last version of salad-cli

--- specialization/data_engineering_with_rust/week-1/salad-cli/src/main.rs
+++ specialization/data_engineering_with_rust/week-1/salad-cli/src/main.rs
@@ -1,19 +1,32 @@
+use indexmap::IndexSet;
 use clap::Parser;
 use salad_cli::create_fruit_salad;
 
 #[derive(Parser)]
 #[clap(
     version = "1.0",
-    author = "Your Name <your.email@example.com>",
-    about = "Number of fruits to include in the salad"
+    author = "Leandro Tiburske <tiburskeleandro@gmail.com>",
+    about = "Fruit salad generator"
 )]
 struct Opts {
     #[clap(short, long)]
     number: usize,
+
+    #[clap(short, long)]
+    fruits: Option<Vec<String>>,
 }
 
 fn main() {
     let opts: Opts = Opts::parse();
+
+    let fruits: Vec<String>;
+
+    if opts.fruits.is_some() {
+        fruits = opts.fruits.unwrap();
+        println!("{:?}", fruits);
+    } else {
+        fruits = [].to_vec();
+    }
 
     // Get the number of fruits the user requested
     let num_fruits = opts.number;
@@ -21,16 +34,19 @@ fn main() {
     // Create the fruit salad
     let result = create_fruit_salad(num_fruits);
     let mut result = result.iter().map(|s| &s[..]).collect::<Vec<&str>>();
+    if fruits.len() > 0 {
+        result.append(&mut fruits.iter().map(|s| &s[..]).collect::<Vec<&str>>());
+    }
 
     // Sort the fruit salad alphabetically
     result.sort_by(|a, b| a.to_lowercase().cmp(&b.to_lowercase()));
 
+    // Remove duplicated fruits
+    let result:IndexSet<&str> = result.iter().cloned().collect();
 
     // Print the fruit salad in human readable format with a count of fruits used
     println!(
         "Created Fruit salad with {} fruits: {:?}",
-
-        num_fruits,
-        result
+        num_fruits, result
     );
 }
